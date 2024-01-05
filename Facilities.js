9596590e59ba13c150d541bb6059b27a6c3bd190
arifazman03 <arif.azman@live.iium.edu.my>
2024-01-05 17:51:54 +0800

    update Facilities.js, img animation

---
+++ Facilities.js
@@ -0,0 +1,16 @@
+const images = document.querySelectorAll('img');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('in-view');
+    } else {
+      entry.target.classList.remove('in-view');
+    }
+  });
+});
+
+images.forEach((image) => {
+  observer.observe(image);
+});
+
